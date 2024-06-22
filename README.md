fe8461ea02af766a1cc9b66ba94a780ebe474eb9
arfanovi <arfanovi.gub1@gmail.com>
2024-06-22 23:49:05 +0600

    Concept or Examples 2

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 * 1. Write a function named 'calculateSum' that takes two arguments and return their Sum.
+
+* Write a function named 'isEven' that takes one argument and returns true if the number is even, and false otherwise
--- index.js
+++ index.js
@@ -5,3 +5,17 @@ function calculateSum(number1 , number2){
     return number1 + number2;
 }
 console.log(calculateSum(5, 5))
+
+//Write a function named ‘isEven’ that takes one argument and returns true if the number is even, and false otherwise.
+
+function isEven(numbers){
+    if (numbers % 2 ===0){
+        return true;
+    } else {
+        return false;
+    }
+}
+const number = isEven(3);
+console.log(number)
+
+
