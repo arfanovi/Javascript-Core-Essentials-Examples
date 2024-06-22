3ab7ddeff4fbbc4cd07cd3f506b0796afb1d674a
arfanovi <arfanovi.gub1@gmail.com>
2024-06-22 23:40:34 +0600

    Concept or Examples 1

---
+++ index.js
@@ -0,0 +1,7 @@
+
+// 1. Write a function named 'calculateSum' that takes two arguments and return their Sum.
+
+function calculateSum(number1 , number2){
+    return number1 + number2;
+}
+console.log(calculateSum(5, 5))
